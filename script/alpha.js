6d503c01e7b5de3077cbadd874eeaff9d2fac738
hasanalam24 <hasanalam365@gmail.com>
2024-02-14 23:57:29 +0600

    matched key

--- script/alpha.js
+++ script/alpha.js
@@ -10,6 +10,31 @@
 // }
 
 
+function handleKeyboardButtonPress(event) {
+    const playerPressed = event.key;
+    // console.log('player Pressed', playerPressed);
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase()
+    // console.log(currentAlphabetElement.innerText)
+    // console.log(playerPressed, expectedAlphabet)
+
+
+    // check matched or not
+    if (playerPressed === expectedAlphabet) {
+        console.log('you win')
+    }
+    else {
+        console.log('your missed . and you loss a life')
+    }
+}
+
+// capture key
+document.addEventListener('keyup', handleKeyboardButtonPress)
+
+
 // eta 2nd korsi
 function continueGame() {
     //step-1: generate a random alphabet
